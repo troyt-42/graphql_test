ce9da2b652080bfe43ac7f1185b5baec0e4a1ee1
Tao_Mac <rnm2888@gmail.com>
2017-04-29 21:09:44 -0400

    Add graphql example codes

---
+++ hello.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+var { graphql, buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
